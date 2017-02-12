021c3dde460b325cc34e12ea338bce6c96ce6662
Alexandre <alex>
2017-02-12 15:21:39 -0500

    change between activities functional

---
+++ TP3/app/src/main/java/ca/csf/mobile2/tp3/DayRemindersActivity.java
@@ -0,0 +1,25 @@
+package ca.csf.mobile2.tp3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_day_selected)
+public class DayRemindersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void createNewReminder(View view) {
+        Intent createNewReminder = new Intent(getApplicationContext(), CreateNewReminderActivity_.class);
+        startActivity(createNewReminder);
+        finish();
+    }
+}
