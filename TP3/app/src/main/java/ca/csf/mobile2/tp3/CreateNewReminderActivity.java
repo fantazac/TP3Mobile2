c7d8f12d15614df62a8d721d9927c618c1dce7e5
Alexandre <alex>
2017-02-13 10:04:11 -0500

    link between activities + dates

--- TP3/app/src/main/java/ca/csf/mobile2/tp3/CreateNewReminderActivity.java
+++ TP3/app/src/main/java/ca/csf/mobile2/tp3/CreateNewReminderActivity.java
@@ -3,8 +3,10 @@ package ca.csf.mobile2.tp3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import org.androidannotations.annotations.EActivity;
@@ -17,16 +19,28 @@ public class CreateNewReminderActivity extends AppCompatActivity {
     protected Button notImportantButton;
     protected Button importantButton;
     protected Button veryImportantButton;
+    protected TextView newDateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     protected void injectViews(@ViewById(R.id.timePicker) TimePicker timePicker,
                                @ViewById(R.id.notImportantButton) Button notImportantButton,
                                @ViewById(R.id.importantButton) Button importantButton,
-                               @ViewById(R.id.veryImportantButton) Button veryImportantButton){
+                               @ViewById(R.id.veryImportantButton) Button veryImportantButton,
+                               @ViewById(R.id.newDateTextView) TextView newDateTextView){
         this.timePicker = timePicker;
         timePicker.setIs24HourView(true);
 
@@ -35,6 +49,9 @@ public class CreateNewReminderActivity extends AppCompatActivity {
 
         this.importantButton = importantButton;
         this.veryImportantButton = veryImportantButton;
+
+        this.newDateTextView = newDateTextView;
+        newDateTextView.setText(getIntent().getStringExtra(MainActivity.SELECTED_DATE));
     }
 
     public void onImportanceButtonClicked(View view) {
@@ -53,17 +70,7 @@ public class CreateNewReminderActivity extends AppCompatActivity {
         }
     }
 
-
-
     public void returnToDaySelected(View view) {
-        Intent returnToDaySelected = new Intent(getApplicationContext(), DayRemindersActivity_.class);
-        startActivity(returnToDaySelected);
-        finish();
-    }
-
-    public void newReminderBack(View view) {
-        Intent returnToDaySelected = new Intent(getApplicationContext(), DayRemindersActivity_.class);
-        startActivity(returnToDaySelected);
         finish();
     }
 }
--- TP3/app/src/main/java/ca/csf/mobile2/tp3/DayRemindersActivity.java
+++ TP3/app/src/main/java/ca/csf/mobile2/tp3/DayRemindersActivity.java
@@ -3,29 +3,66 @@ package ca.csf.mobile2.tp3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TimePicker;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @EActivity(R.layout.activity_day_selected)
 public class DayRemindersActivity extends AppCompatActivity {
 
+    protected TextView dateTextView;
+    protected Date selectedDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    protected void injectViews(@ViewById(R.id.dateTextView) TextView dateTextView){
+        this.dateTextView = dateTextView;
+        selectedDate = new Date(getIntent().getLongExtra(MainActivity.SELECTED_DATE_UTC, -1));
+        dateTextView.setText(getCurrentDay(selectedDate));
+    }
+
+    protected String getCurrentDay(Date date) {
+        java.text.DateFormat dateFormat = DateFormat.getLongDateFormat(this);
+
+        String restOfDate = dateFormat.format(date);
+        int indexOfFirstLetterOfMonth = restOfDate.indexOf(' ') + 1;
+        restOfDate = restOfDate.substring(0, indexOfFirstLetterOfMonth) +
+                restOfDate.substring(indexOfFirstLetterOfMonth, indexOfFirstLetterOfMonth+1).toUpperCase() +
+                restOfDate.substring(indexOfFirstLetterOfMonth+1);
+
+        return getDayOfTheWeek(date) + ", " + restOfDate;
+    }
+
+    protected String getDayOfTheWeek(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE");
+        String dayOfTheWeek = simpleDateFormat.format(date);
+        return dayOfTheWeek.substring(0, 1).toUpperCase() + dayOfTheWeek.substring(1);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void createNewReminder(View view) {
         Intent createNewReminder = new Intent(getApplicationContext(), CreateNewReminderActivity_.class);
+        createNewReminder.putExtra(MainActivity.SELECTED_DATE, dateTextView.getText());
+        createNewReminder.putExtra(MainActivity.SELECTED_DATE_UTC, selectedDate.getTime());
         startActivity(createNewReminder);
-        finish();
-    }
-
-    public void daySelectedBack(View view) {
-        Intent main = new Intent(getApplicationContext(), MainActivity_.class);
-        startActivity(main);
-        finish();
     }
 }
